a895010d42af57286ffef386dc6fec1212db2064
Ahmed Nour <mimianwar33@hotmail.com>
2023-11-20 06:49:44 +0200

    folder and files struture

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+namespace PHPMVC;
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>mido anwar</h1>
+</body>
+</html>
