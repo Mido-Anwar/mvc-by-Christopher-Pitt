3c52fa8a2f2532e5bcc1908e9b32bdd53a41682b
Mido-Anwar <mimianwar33@hotmail.com>
2024-10-17 20:30:31 +0300

    Redirecting to Another URL

--- includes/404.php
+++ includes/404.php
@@ -1,17 +1,11 @@
-<?php
-namespace PHPMVC;
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>404</title>
 </head>
 <body>
-    <h1>mido anwar</h1>
+    <h1>404 not found</h1>
 </body>
 </html>
